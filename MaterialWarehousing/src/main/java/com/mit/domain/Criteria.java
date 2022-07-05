f221e553373d010e540769e4fe836cb61d5128a0
ChoiJeongEun <c_car@daum.net>
2022-07-05 16:10:57 +0900

    criteria 생성

---
+++ MaterialWarehousing/src/main/java/com/mit/domain/Criteria.java
@@ -0,0 +1,23 @@
+package com.mit.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Criteria {
+	//페이지 번호, 한페이지당 개수
+	private int pageNum=1;
+	private int amount=10;
+	
+	//기본생성자 ->기본값 1,10
+	public Criteria() {
+		
+	}
+	//추가 생성자 -> 값을 넣어서 만들기 
+}
+
+
+
+
+
