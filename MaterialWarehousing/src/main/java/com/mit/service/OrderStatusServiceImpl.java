c4a2c20909fc1575a80cc7b7870839ad065d2ee5
dain <dtp05093@naver.com>
2022-07-05 18:04:42 +0900

    version 0.27
    - 오류수정
    - (OrderStatusServiceImpl에 어노테이션 누락 -> 추가함)

--- MaterialWarehousing/src/main/java/com/mit/service/OrderStatusServiceImpl.java
+++ MaterialWarehousing/src/main/java/com/mit/service/OrderStatusServiceImpl.java
@@ -2,9 +2,15 @@ package com.mit.service;
 
 import java.util.List;
 
-import com.mit.domain.OrderStatusVO;
+import org.springframework.stereotype.Service;
 import com.mit.mapper.OrderStatusMapper;
 
+import com.mit.domain.OrderStatusVO;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class OrderStatusServiceImpl implements OrderStatusService {
 
 	private OrderStatusMapper mapper;
