53a37df0e2edf48bb201146f8fe65e4aee534a23
kimyunjeong <youwjd51@gmail.com>
2022-07-07 17:46:52 +0900

    version 0.33
    --거래명세서 발주번호별 DB 연동

--- MaterialWarehousing/src/main/java/com/mit/service/transactionCloseServiceImpl.java
+++ MaterialWarehousing/src/main/java/com/mit/service/transactionCloseServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.mit.domain.statementVO;
 import com.mit.domain.transactionCloseVO;
 import com.mit.mapper.transactionCloseMapper;
 
@@ -21,9 +20,14 @@ public class transactionCloseServiceImpl implements transactionCloseService {
 	return mapper.getList();
 	}
 	
+//	@Override
+//	public List<statementVO> List(Long order_num) {
+//	return mapper.List(order_num);
+//	}
 	@Override
-	public List<statementVO> List() {
-	return mapper.List();
+	public transactionCloseVO get(Long order_num) {
+		return mapper.read(order_num) ;//db에접속해서 가져온다. 
+		
 	}
 
 
